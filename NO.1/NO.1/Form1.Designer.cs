2c0b93571b6651dce50b36931be434e1a5d0d420
Gio-Dha <giovany5729v@gmail.com>
2025-10-31 19:45:34 -0600

    Add files via upload

---
+++ NO.1/NO.1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace NO._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(111, 89);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 259);
+            listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Emoji", 12F, FontStyle.Bold);
+            label1.Location = new Point(205, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(256, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Escribe los números del 0 al 10\r\n";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(404, 208);
+            button1.Name = "button1";
+            button1.Size = new Size(80, 31);
+            button1.TabIndex = 2;
+            button1.Text = "Listar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Label label1;
+        private Button button1;
+    }
+}
