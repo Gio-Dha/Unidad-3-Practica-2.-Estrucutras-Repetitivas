60adb2e6f2c08ac1e4096031f19b35000abdc655
Gio-Dha <giovany5729v@gmail.com>
2025-10-31 19:46:38 -0600

    Add files via upload

---
+++ NO.3/NO.3/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace NO._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Emoji", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(230, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(471, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Este programa suma los numeros capturados";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(169, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(125, 17);
+            label2.TabIndex = 1;
+            label2.Text = "Introduce un número";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(295, 102);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LightPink;
+            button2.Location = new Point(295, 144);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 24);
+            button2.TabIndex = 3;
+            button2.Text = "Añadir";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightPink;
+            button1.Location = new Point(39, 254);
+            button1.Name = "button1";
+            button1.Size = new Size(89, 51);
+            button1.TabIndex = 4;
+            button1.Text = "Sumar\r\nnúmeros\r\n";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(184, 254);
+            label3.Name = "label3";
+            label3.Size = new Size(132, 17);
+            label3.TabIndex = 5;
+            label3.Text = "La suma de todos son:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(322, 248);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(646, 144);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 94);
+            listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(990, 450);
+            Controls.Add(listBox1);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox2;
+        private ListBox listBox1;
+    }
+}
